f4a5f5755f9930af56ec7c15602f18d14547141c
Станислав Маяцкий <stasm226@gmail.com>
2025-03-24 15:59:12 +0500

    Add owner field to Course & Lewsson models

--- materials/models.py
+++ materials/models.py
@@ -7,6 +7,7 @@ class Course(models.Model):
     name = models.CharField(max_length=255, verbose_name="Название курса")
     description = models.TextField(verbose_name="Описание курса")
     image = models.ImageField(upload_to="courses/", blank=True, null=True, verbose_name="Превью курса")
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE, blank=True, null=True, verbose_name="Владелец курса")
 
     def __str__(self):
         """Метод для отображения объекта курса в админке."""
@@ -27,6 +28,7 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons", verbose_name="Курс")
     image = models.ImageField(upload_to="lessons/", blank=True, null=True, verbose_name="Превью урока")
     video = models.FileField(upload_to="lessons/", blank=True, null=True, verbose_name="Видео урока")
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE, blank=True, null=True, verbose_name="Владелец урока")
 
     def __str__(self):
         """Метод для отображения объекта урока в админке."""
